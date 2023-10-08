c43b7483815391d17817578bd2ed384f27be3512
Bella <bella.kallen2014@gmail.com>
2023-10-09 01:53:21 +0300

    scr added

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int amount = 10_000;
+        int mile = 20;
+        int bonusMiles = amount / mile;
+
+        System.out.println(" Бонусные мили: " + bonusMiles);
+    }
+}
